f53f4c912d0681303560bf167699cdca9aa6272f
GetMyIsland <getmyisland@gmail.com>
2022-04-22 14:58:53 +0200

    Added first version of random fact printing

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from discord.ext import commands
 import os
 from dotenv import load_dotenv
 import random
-import numpy
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
@@ -67,6 +68,22 @@ async def coinflip(ctx):
         await ctx.send('Heads')
     else:
         await ctx.send('Tails')
+
+
+@bot.command(description='Tells a random fact')
+async def randomfact(ctx):
+    """Return a random fact."""
+    driver = webdriver.Firefox()
+    driver.get('https://www.google.com/search?client=firefox-b-d&q=i%27m+feeling+curious&ved=2ahUKEwiz9c-C1Kf3AhXGAewKHaJHBMAQ73N6BAgKEA0')
+    element = driver.find_element_by_class_name("sW6dbe")
+    fact_title = element.text
+    element_fact = driver.find_element_by_class_name("EikfZ IFC-animate")
+    fact = element_fact.text
+    driver.close()
+
+    fullfact = fact_title + '\n' + fact
+    print(fullfact)
+    await ctx.send(fullfact)
 
 
 bot.run(TOKEN)
