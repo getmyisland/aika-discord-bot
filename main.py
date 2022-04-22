f06069c05d2b6eadb383c568a3a3f8a8e2f3f0e4
GetMyIsland <getmyisland@gmail.com>
2022-04-22 11:54:24 +0200

    Added message listener and .env file

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print('Hello World')
+import discord
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv('TOKEN')
+
+class CreateClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        if message.author == client.user:
+            return
+
+        print('Message from {0.author}: {0.content}'.format(message))
+
+        if message.content.lower().startswith('hello'):
+            await message.channel.send('Hello!')
+
+client = CreateClient()
+client.run(TOKEN)
