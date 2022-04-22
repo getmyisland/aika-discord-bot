07f55be02a3e8b5ecf4ad67aa19c57eadc87f520
getmyisland <getmyisland@gmail.com>
2022-04-22 20:50:03 +0200

    Renamed main.py to bot.py

--- bot.py
+++ bot.py
@@ -3,8 +3,6 @@ from discord.ext import commands
 import os
 from dotenv import load_dotenv
 import random
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
@@ -70,20 +68,8 @@ async def coinflip(ctx):
         await ctx.send('Tails')
 
 
-@bot.command(description='Tells a random fact')
-async def randomfact(ctx):
-    """Return a random fact."""
-    driver = webdriver.Firefox()
-    driver.get('https://www.google.com/search?client=firefox-b-d&q=i%27m+feeling+curious&ved=2ahUKEwiz9c-C1Kf3AhXGAewKHaJHBMAQ73N6BAgKEA0')
-    element = driver.find_element_by_class_name("sW6dbe")
-    fact_title = element.text
-    element_fact = driver.find_element_by_class_name("EikfZ IFC-animate")
-    fact = element_fact.text
-    driver.close()
-
-    fullfact = fact_title + '\n' + fact
-    print(fullfact)
-    await ctx.send(fullfact)
-
+@bot.command(description="See all commands")
+async def commands(ctx):
+    await ctx.send('Coming Soon!')
 
 bot.run(TOKEN)
